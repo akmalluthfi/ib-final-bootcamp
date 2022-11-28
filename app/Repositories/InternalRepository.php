2508d1c27d8d13f50e935347da73f87e87ad17ae
rhaikal <rhaikal101@gmail.com>
2022-11-28 20:57:18 +0700

    menghapus function yang tidak penting di internal & memperbaiki internal delete attachments

--- app/Repositories/InternalRepository.php
+++ app/Repositories/InternalRepository.php
@@ -4,17 +4,6 @@ namespace App\Repositories;
 
 class InternalRepository
 {
-    public function create($data, $instruction)
-    {
-        $dataSaved = [
-            'attachments' => [$data['attachment']]
-        ];
-
-        $internal = $instruction->internal()->create($dataSaved);
-
-        return $internal;
-    }
-
     public function update($data, $internal)
     {
         $internal->update($data);
